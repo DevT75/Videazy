71675cc030401fce498187c59992906a1358062a
Devendra <dekuta75@gmail.com>
2024-07-18 18:32:11 +0530

    port int typecast

--- backend/main.py
+++ backend/main.py
@@ -56,5 +56,5 @@ def delete_file(file_path: str):
 
 if __name__ == "__main__":
     import uvicorn
-    port = os.environ.get("PORT", 5000)
+    port = int(os.environ.get("PORT", 5000))
     uvicorn.run("main:app", host="0.0.0.0", port=port,reload=True)
