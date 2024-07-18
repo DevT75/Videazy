bb1ff118ba4cfedad4e606fc07fd3a245ce11fd7
Devendra <dekuta75@gmail.com>
2024-07-18 17:19:20 +0530

    db uri changes

--- backend/core/config.py
+++ backend/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     DB_NAME: str = os.getenv('MYSQL_DB')
     DB_HOST: str = os.getenv('MYSQL_SERVER')
     DB_PORT: str = os.getenv('MYSQL_PORT')
-    DATABASE_URL: str = f"mysql+pymysql://{DB_USER}:%s@{DB_HOST}:{DB_PORT}/{DB_NAME}"% quote_plus(DB_PASSWORD)
+    DATABASE_URL: str = os.getenv('MYSQL_URI') or f"mysql+pymysql://{DB_USER}:%s@{DB_HOST}:{DB_PORT}/{DB_NAME}"% quote_plus(DB_PASSWORD)
     JWT_SECRET:str = os.getenv('JWT_SECRET','8e6f560a9fc1ffbf82e2e1e5ecc4fccd7cfa4a5d503a11b5802c080512aaf8b8')
     JWT_ALGORITHM: str = os.getenv('JWT_ALGORITHM','HS256')
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv('JWT_TOKEN_EXPIRE_MINUTES',60)
