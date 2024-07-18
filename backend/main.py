09e4d19c63043f7e9f5aae5ec1a69e691ad6d128
Devendra <dekuta75@gmail.com>
2024-07-18 20:54:44 +0530

    fix again

--- backend/main.py
+++ backend/main.py
@@ -57,4 +57,4 @@ def delete_file(file_path: str):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("app", host="0.0.0.0", port=port,reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port,reload=True)
