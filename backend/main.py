6b71925f4b0eddf158c88c3aba72eb58c408fa58
Devendra <dekuta75@gmail.com>
2024-07-19 13:12:46 +0530

    attempt no 420 fixing cors

--- backend/main.py
+++ backend/main.py
@@ -1,52 +1,24 @@
 import os
 from fastapi import FastAPI, HTTPException, BackgroundTasks
 from fastapi.responses import FileResponse, JSONResponse
-from users.route import router as user_router,token_router
+from users.route import router as user_router, token_router
 from auth.route import router as auth_router
 from core.security import JWTAuth
 from starlette.middleware.authentication import AuthenticationMiddleware
 from video.route import router as upload_router
-from video.route import compressed_folder,upload_folder
+from video.route import compressed_folder, upload_folder
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-FRONTEND_URL = "https://videazy.vercel.app"
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:5173",
-    "https://videazy.vercel.app",
-    "https://videazy-devt75s-projects.vercel.app",
-    "https://videazy-aifji3oem-devt75s-projects.vercel.app"
-]
-
+# Use wildcard for CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://videazy.vercel.app"],
+    allow_origins=["*"],  # Allow all origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
 )
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-@app.middleware("http")
-async def add_cors_headers(request, call_next):
-    response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    response.headers["Access-Control-Allow-Methods"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-    return response
 
 import logging
 
@@ -64,25 +36,22 @@ app.include_router(auth_router)
 app.include_router(token_router)
 app.include_router(upload_router)
 
-app.add_middleware(AuthenticationMiddleware,backend=JWTAuth())
+app.add_middleware(AuthenticationMiddleware, backend=JWTAuth())
 
 @app.get("/")
 async def home():
-    return {"Response" : "Hello World!!!"}
+    return {"Response": "Hello World!!!"}
 
 @app.get("/download/{filename}")
 async def download_file(filename: str, background_tasks: BackgroundTasks):
     file_path = os.path.join(compressed_folder, filename)
-    upload_file_name = filename.replace('compressed_','')
+    upload_file_name = filename.replace('compressed_', '')
     upload_file_path = os.path.join(upload_folder, upload_file_name)
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
     background_tasks.add_task(delete_file, file_path)
     background_tasks.add_task(delete_file, upload_file_path)
-    response = FileResponse(path=file_path, filename=filename)
-    response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
+    return FileResponse(path=file_path, filename=filename)
 
 def delete_file(file_path: str):
     if os.path.exists(file_path):
@@ -91,14 +60,9 @@ def delete_file(file_path: str):
 
 @app.options("/upload/")
 async def options_upload():
-    response = JSONResponse(content={"message": "OK"})
-    response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
-    response.headers["Access-Control-Allow-Methods"] = "POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
+    return JSONResponse(content={"message": "OK"})
 
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port,reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
--- backend/video/route.py
+++ backend/video/route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,status,Depends,Request,UploadFile,File
+from fastapi import APIRouter, status, Depends, Request, UploadFile, File
 from typing import List
 from fastapi.responses import FileResponse, JSONResponse
 from core.db import get_db
@@ -7,7 +7,7 @@ from users.models import UserModel
 from video.model import MediaModel
 from core.security import get_current_user, oauth2_scheme
 from sqlalchemy.orm import Session
-from typing import Annotated,Union
+from typing import Annotated, Union
 from pathlib import Path
 import cloudinary
 import cloudinary.uploader
@@ -23,8 +23,6 @@ import sys
 env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
-FRONTEND_URL = "https://videazy.vercel.app"
-
 cloudinary.config(
     cloud_name = os.getenv('CLOUDINARY_NAME'),
     api_key = os.getenv('CLOUDINARY_KEY'),
@@ -34,7 +32,6 @@ cloudinary.config(
 router = APIRouter(
     prefix="/upload",
     tags=["Upload"],
-    # dependencies=[Depends(oauth2_scheme)]
 )
 
 global_file_size = 0
@@ -48,13 +45,12 @@ async def check_files_size(request: Request, files: List[UploadFile]):
         file.file.seek(0, os.SEEK_END)
         file_size = file.file.tell()
         total_size += file_size
-        # global_file_size = file_size
         file.file.seek(0)  # Reset file pointer
     if total_size > max_size:
         raise HTTPException(status_code=413, detail="Total file size exceeds the limit")
     return files
 
-ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'mkv','webm'}
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'mkv', 'webm'}
 
 codec_options = {
     'vp8_vorbis': {
@@ -80,20 +76,18 @@ codec_options = {
 }
 
 def get_codec_options(codec_name):
-    # Map codec names to codec options and presets
     if codec_name == 'h264':
         options = codec_options['h264_aac']
-        preset = '-preset fast'  # Example preset for H.264
+        preset = '-preset fast'
     elif codec_name == 'vp9':
         options = codec_options['vp9_opus']
-        preset = '-preset ultrafast'  # Example preset for VP9
+        preset = '-preset ultrafast'
     elif codec_name == 'vp8':
         options = codec_options['vp8_vorbis']
-        preset = '-preset veryfast'  # Example preset for VP8
+        preset = '-preset veryfast'
     else:
-        # Default to VP8/Vorbis for unknown codecs
         options = codec_options['vp8_vorbis']
-        preset = '-preset veryfast'  # Example preset for unknown codecs
+        preset = '-preset veryfast'
     return options, preset
 
 upload_folder = 'static/uploads'
@@ -107,32 +101,27 @@ def get_length(filename):
         stderr=subprocess.STDOUT)
     return float(result.stdout)
 
-
-@router.post("/",status_code=status.HTTP_200_OK)
-async def upload_media_unauthenticated(req:Request,files: List[UploadFile]):
+@router.post("/", status_code=status.HTTP_200_OK)
+async def upload_media_unauthenticated(req: Request, files: List[UploadFile]):
     results = []
     for file in files:
         if not file.filename.endswith(tuple(ALLOWED_EXTENSIONS)):
             raise HTTPException(status_code=400, detail="Invalid file type")
         result = await compress_video(file=file)
         results.append(result)
-    response = JSONResponse(content=results)
-    response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
+    return JSONResponse(content=results)
 
-@router.post("/me",status_code=status.HTTP_200_OK)
-async def upload_media_authenticated(req:Request,current_user:Annotated[UserModel,Depends(get_current_user)],files:List[UploadFile] = Depends(check_files_size),db:Session = Depends(get_db)):
+@router.post("/me", status_code=status.HTTP_200_OK)
+async def upload_media_authenticated(req: Request, current_user: Annotated[UserModel, Depends(get_current_user)], files: List[UploadFile] = Depends(check_files_size), db: Session = Depends(get_db)):
     results = []
     for file in files:
         if not file.filename.endswith(tuple(ALLOWED_EXTENSIONS)):
             raise HTTPException(status_code=400, detail="Invalid file type")
         compressed_video = await compress_video(file=file)
-        compressed_file_path = os.path.join(compressed_folder,compressed_video["filename"])
-        upload_response = cloudinary.uploader.upload(compressed_file_path, resource_type="auto",public_id=f"compressed_{file.filename}{req.user.id}")
+        compressed_file_path = os.path.join(compressed_folder, compressed_video["filename"])
+        upload_response = cloudinary.uploader.upload(compressed_file_path, resource_type="auto", public_id=f"compressed_{file.filename}{req.user.id}")
         cloudinary_video_url = upload_response['secure_url']
-        # Save video metadata in the database
-        compressed_video= MediaModel(
+        compressed_video = MediaModel(
                 user_id = req.user.id,
                 type=file.headers["content-type"].split('/')[0],
                 url=cloudinary_video_url,
@@ -145,12 +134,7 @@ async def upload_media_authenticated(req:Request,current_user:Annotated[UserMode
             "filename": file.filename,
             "download_url": f"/download/{f'compressed_{file.filename}'}"
         })
-    response = JSONResponse(content=results)
-    response.headers["Access-Control-Allow-Origin"] = FRONTEND_URL
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
-
-
+    return JSONResponse(content=results)
 
 async def compress_video(file: UploadFile):
     os.makedirs(upload_folder, exist_ok=True)
@@ -165,22 +149,19 @@ async def compress_video(file: UploadFile):
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
-
-    ffmpeg_command = [ 'ffmpeg', '-i', file_path ]
+    ffmpeg_command = ['ffmpeg', '-i', file_path]
     file_codec = ffmpeg.probe(file_path)["streams"][0]["codec_name"]
-    options,preset = get_codec_options(file_codec)
+    options, preset = get_codec_options(file_codec)
     for key, value in options.items():
         ffmpeg_command.extend(['-{}'.format(key), value])
     file_duration = get_length(file_path)
     print(file_duration)
-    ffmpeg_command.extend(['-crf','24'])
-    # preset options
+    ffmpeg_command.extend(['-crf', '24'])
     ffmpeg_command.extend(preset.split())
     ffmpeg_command.append(compressed_file_path)
 
     try:
         subprocess.run(ffmpeg_command, check=True)
         return {"filename": f"compressed_{file.filename}", "url": f"/download/{f'compressed_{file.filename}'}"}
-
     except subprocess.CalledProcessError as e:
         raise HTTPException(status_code=500, detail=f"Error occurred while compressing video: {e}")
