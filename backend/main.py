85ab3861d40c19b1ed99341044424586cce39ad5
Devendra <dekuta75@gmail.com>
2024-07-19 12:04:57 +0530

    updated origins

--- backend/main.py
+++ backend/main.py
@@ -16,12 +16,14 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:5173",
-    "https://videazy.vercel.app/"
+    "https://videazy.vercel.app/",
+    "https://videazy-devt75s-projects.vercel.app/",
+    "https://videazy-aifji3oem-devt75s-projects.vercel.app/"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
